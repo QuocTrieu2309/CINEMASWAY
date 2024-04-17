9e27b2c2374a474448912c7c9f6e0c2dc2266efc
quoctrieu2309 <trieunvph30369@fpt.edu.vn>
2024-04-17 20:12:47 +0700

    Add_Permissions_Model

---
+++ app/Models/Permission.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Permission extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'name',
+        'description',
+        'deleted'
+   ];
+   protected $hidden = [
+       'created_by',
+       'updated_by',
+       'created_at',
+       'updated_at',
+   ];
+}

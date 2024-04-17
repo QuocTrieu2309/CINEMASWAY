3a197d8c0a2149c8eb7ad2722f0827d09317cf0f
quoctrieu2309 <trieunvph30369@fpt.edu.vn>
2024-04-17 20:29:49 +0700

    Add_Migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,18 +15,25 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        \App\Models\User::factory()->create([
-            'full_name' => 'Quốc Triệu',
-            'role_id' => 1,
-            'email' => 'cristianotrieulan@gmail.com',
-            'phone' => '0364122323',
-            'password' => Hash::make('23091999'),
-            'gender' => \App\Models\User::GENDER_MALE,
-            'birth_date' => now(),
-            'avatar' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0qj2hty4nA_9f4S4LeOJBNhHOUX6x99QdUfzWQC3QKLAEobv97QXhP3Oe6ocZhTnhlPU&usqp=CAU',
+        // \App\Models\User::factory()->create([
+        //     'full_name' => 'Quốc Triệu',
+        //     'role_id' => 1,
+        //     'email' => 'cristianotrieulan@gmail.com',
+        //     'phone' => '0364122323',
+        //     'password' => Hash::make('23091999'),
+        //     'gender' => \App\Models\User::GENDER_MALE,
+        //     'birth_date' => now(),
+        //     'avatar' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0qj2hty4nA_9f4S4LeOJBNhHOUX6x99QdUfzWQC3QKLAEobv97QXhP3Oe6ocZhTnhlPU&usqp=CAU',
+        // ]);
+
+        // \App\Models\Role::query()->create([
+        //     'name'=> "Quản trị viên",
+        // ]);
+
+        \App\Models\Permission::query()->create([
+            'user_id'=> 6,
+            'name'=> "Quản lý quyền hạn",
         ]);
-        \App\Models\Role::query()->create([
-            'name'=> "Quản trị viên",
-        ]);
+
     }
 }

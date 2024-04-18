a8e1b2fd5cf49f15eae3ad55a161d02f36255416
quoctrieu2309 <trieunvph30369@fpt.edu.vn>
2024-04-18 10:02:26 +0700

    Add_User_Permission_Migrate

--- database/migrations/2024_04_18_012938_create_user_permissions_table.php
+++ database/migrations/2024_04_18_012938_create_user_permissions_table.php
@@ -13,7 +13,14 @@ return new class extends Migration
     {
         Schema::create('user_permissions', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('permission_id');
+            $table->unsignedBigInteger('created_by')->nullable();
+            $table->unsignedBigInteger('updated_by')->nullable();
+            $table->integer('deleted')->default(0);
             $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('permission_id')->references('id')->on('permissions');
         });
     }
 

456f22e14aa0ff41a1d02b8d0a8cc8a7e05b49a3
Trinh Viet Duc <ductv@vnext.com>
2024-05-20 22:31:59 +0700

    Create model Transaction

---
+++ app/Models/Transaction.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Transaction extends Model
+{
+    use HasFactory;
+
+
+    protected $fillable = [
+        'name',
+        'deleted'
+    ];
+    protected $hidden = [
+        'created_by',
+        'updated_by',
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     *
+     * @return BelongsTo
+     */
+    public function booking(){
+        return $this->belongsTo(Booking::class);
+    }
+}

1c1491a31ef0e3a8c2c0207cce140273dc745709
koko28011996 <oke.anhlathangbech@gmail.com>
2024-05-21 23:58:21 +0700

    BookingServiceResource

---
+++ app/Http/Resources/API/BookingService/BookingServiceResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources\Api\BookingService;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BookingServiceResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'booking_id' => $this->booking_id,
+            'service_id' => $this->service_id,
+            'subtotal' => $this->subtotal,
+            'quantity' => $this->quantity,
+        ];
+    }
+}

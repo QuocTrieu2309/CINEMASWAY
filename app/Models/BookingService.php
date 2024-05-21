161517f011121ca26fc72f041dfd794e521877d5
koko28011996 <oke.anhlathangbech@gmail.com>
2024-05-21 23:38:44 +0700

    Model BookingService

---
+++ app/Models/BookingService.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use App\Policies\ServicePolicy;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BookingService extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'booking_id',
+        'services_id',
+        'quantity',
+        'subtotal',
+
+    ];
+    protected $hidden = [
+        'created_by',
+        'updated_by',
+        'created_at',
+        'updated_at',
+    ];
+    public function bookings()
+    {
+        return $this->belongsTo(Booking::class);
+    }
+    public function services()
+    {
+        return $this->belongsTo(ServicePolicy::class);
+    }
+}

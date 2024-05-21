1501f4a45c2fdfe1bfa7f524c886b3eeb57ae32d
koko28011996 <oke.anhlathangbech@gmail.com>
2024-05-22 00:01:12 +0700

    BookingServicePolicy

---
+++ app/Policies/BookingServicePolicy.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+use Illuminate\Http\Response as HttpResponse;
+class BookingServicePolicy
+{
+    /**
+     * Create a new policy instance.
+     */
+    public function checkPermission(User $user)
+    {
+        return CheckPermissionWithPolicy($user,'BookingService')
+            ? Response::allow()
+            : Response::deny('Bạn không có quyền truy cập', HttpResponse::HTTP_FORBIDDEN);
+    }
+    public function delete(User $user){
+        return CheckPermissionWithPolicy($user,'BookingService')
+            ? Response::allow()
+            : Response::deny('Bạn không có quyền truy cập', HttpResponse::HTTP_FORBIDDEN);
+    }
+}

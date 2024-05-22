b194ab52418f96e0e9199ab72de5934231601c0b
PTuanDEV <tuanchay.ko@gmail.com>
2024-05-22 22:59:24 +0700

    create policy

--- app/Policies/BookingPolicy.php
+++ app/Policies/BookingPolicy.php
@@ -2,13 +2,21 @@
 
 namespace App\Policies;
 
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+use Illuminate\Http\Response as HttpResponse;
+
 class BookingPolicy
 {
-    /**
-     * Create a new policy instance.
-     */
-    public function __construct()
+    public function checkPermission(User $user)
     {
-        //
+        return CheckPermissionWithPolicy($user,'Booking')
+            ? Response::allow()
+            : Response::deny('Bạn không có quyền truy cập', HttpResponse::HTTP_FORBIDDEN);
+    }
+    public function delete(User $user){
+        return CheckPermissionWithPolicy($user,'Booking')
+            ? Response::allow()
+            : Response::deny('Bạn không có quyền truy cập', HttpResponse::HTTP_FORBIDDEN);
     }
 }

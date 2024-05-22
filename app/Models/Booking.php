1d1b5d9e994f3276b86684fa29f8702d37877bbe
PTuanDEV <tuanchay.ko@gmail.com>
2024-05-22 22:58:09 +0700

    create model

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -8,4 +8,34 @@ use Illuminate\Database\Eloquent\Model;
 class Booking extends Model
 {
     use HasFactory;
+    protected $table = "bookings";
+    protected $primaryKey = "id";
+    public $timestamps = true;
+    protected $fillable = [
+        'user_id',
+        'ticket_type_id',
+        'showtime_id',
+        'quantity',
+        'subtotal',
+        'status',
+    ];
+    protected $hidden = [
+        'created_by',
+        'updated_by',
+        'deleted',
+        'created_at',
+        'updated_at',
+    ];
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
+    public function ticketTypes()
+    {
+        return $this->hasMany(TicketType::class);
+    }
+    public function showtimes()
+    {
+        return $this->hasMany(Showtime::class);
+    }
 }

8dba7a23d967d1126b0555eaf0f95989d7cfce3c
koko28011996 <oke.anhlathangbech@gmail.com>
2024-06-01 03:37:52 +0700

    CreateMoMo

--- app/Http/Controllers/API/Momo/MomoController.php
+++ app/Http/Controllers/API/Momo/MomoController.php
@@ -4,18 +4,23 @@ namespace App\Http\Controllers\API\Momo;
 
 use App\Http\Controllers\Controller;
 use App\Models\Booking;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
 
 class MomoController extends Controller
 {
+
     public function __construct()
     {
         $this->middleware('auth:sanctum');
     }
-    public function createPayment(Request $request)
+
+
+    public function payment(Request $request)
     {
         $userId = Auth::id();
         $bookingId = $request->input('booking_id');
@@ -24,28 +29,41 @@ class MomoController extends Controller
         if (!$booking) {
             return response()->json(messageResponseNotFound(), Response::HTTP_BAD_REQUEST);
         }
-
         $endpoint = "https://test-payment.momo.vn/v2/gateway/api/create";
         $partnerCode = 'MOMOBKUN20180529';
         $accessKey = 'klm05TvNBzhg7h7j';
         $secretKey = 'at67qH6mk8w5Y1nAyMoYKMWACiEi2bsa';
         $orderInfo = "Thanh toán qua MoMo";
         $amount = intval($booking->subtotal);
-        $orderId = time();
-        $redirectUrl = "https://fpt.edu.vn"; // Đường dẫn chuyển hướng sau khi thanh toán
-        $ipnUrl = "https://abc.com"; // Đường dẫn nhận thông báo sau khi lấy chữ ký IPN từ MoMo
+        $orderId = $partnerCode . Str::uuid();
+        $redirectUrl = "https://abc.com"; // Đường dẫn chuyển hướng sau khi thanh toán
+        $ipnUrl = route('momo.callback'); // Đường dẫn nhận thông báo sau khi lấy chữ ký IPN từ MoMo
         $extraData = "";
-        $requestId = time();
+        $requestId =  $orderId;
         $requestType = "payWithATM";
+        $requestTime = Carbon::now()->timestamp;
+        $expireTime = Carbon::now()->addMinutes(15)->timestamp;
 
-        // Tạo chữ ký hash
-        $rawHash = "accessKey=" . $accessKey . "&amount=" . $amount . "&extraData=" . $extraData . "&ipnUrl=" . $ipnUrl . "&orderId=" . $orderId . "&orderInfo=" . $orderInfo . "&partnerCode=" . $partnerCode . "&redirectUrl=" . $redirectUrl . "&requestId=" . $requestId . "&requestType=" . $requestType;
-        $signature = hash_hmac("sha256", $rawHash, $secretKey);
+        $rawSignature =
+            'accessKey=' . $accessKey .
+            '&amount=' . $amount .
+            '&extraData=' . $extraData .
+            '&ipnUrl=' . $ipnUrl .
+            '&orderId=' . $orderId .
+            '&orderInfo=' . $orderInfo .
+            '&partnerCode=' . $partnerCode .
+            '&redirectUrl=' . $redirectUrl .
+            '&requestId=' . $requestId .
+            '&requestType=' . $requestType;
+            '&requestTime=' .  $requestTime;
+            '&expireTime=' .  $expireTime;
 
-        $data = [
+        $signature = hash_hmac('sha256', $rawSignature, $secretKey);
+
+        $requestBody = [
             'partnerCode' => $partnerCode,
-            'partnerName' => "Test",
-            'storeId' => "MomoTestStore",
+            'partnerName' => 'Test',
+            'storeId' => 'MomoTestStore',
             'requestId' => $requestId,
             'amount' => $amount,
             'orderId' => $orderId,
@@ -53,23 +71,52 @@ class MomoController extends Controller
             'redirectUrl' => $redirectUrl,
             'ipnUrl' => $ipnUrl,
             'lang' => 'vi',
-            'extraData' => $extraData,
             'requestType' => $requestType,
-            'signature' => $signature
+            'extraData' => $extraData,
+            'signature' => $signature,
         ];
 
-        $response = Http::post($endpoint, $data);
-        if ($response->successful()) {
-            $paymentUrl = $response['payUrl'];
-            $booking = Booking::findOrFail($bookingId);
-            if ($booking->status === 'đã thanh toán' || $booking->status === 'đã hủy') {
-                return ApiResponse(false, null, Response::HTTP_BAD_REQUEST, 'Đơn hàng đã được xử lý hoặc đã hủy');
-            } else {
-                $booking->update(['status' => 'đã thanh toán']);
-                return ApiResponse(true, $paymentUrl, Response::HTTP_OK, messageResponseActionSuccess());
-            }
-        } else {
-            return ApiResponse(false, Null, Response::HTTP_BAD_REQUEST, 'yêu cầu thanh toán thất bại'());
+        $response = Http::post('https://test-payment.momo.vn/v2/gateway/api/create', $requestBody);
+
+        return ApiResponse(true, $response->json(), Response::HTTP_OK, messageResponseData());
+    }
+    public function callback(Request $request)
+    {
+        return ApiResponse(true, $request->all(), Response::HTTP_OK, messageResponseActionSuccess());
+    }
+
+    public function checkStatusTransaction(Request $request)
+    {
+        $orderId = $request->input('orderId');
+        $bookingId = $request->input('booking_id');
+        if (!$orderId || !$bookingId) {
+            return ApiResponse(false, [], Response::HTTP_BAD_REQUEST, 'Vui lòng kiểm tra lại');
         }
+
+        $accessKey = 'klm05TvNBzhg7h7j';
+        $secretKey = 'at67qH6mk8w5Y1nAyMoYKMWACiEi2bsa';
+        $rawSignature = "accessKey={$accessKey}&orderId={$orderId}&partnerCode=MOMOBKUN20180529&requestId={$orderId}";
+        $signature = hash_hmac('sha256', $rawSignature, $secretKey);
+        $requestBody = [
+            'partnerCode' => 'MOMOBKUN20180529',
+            'requestId' => $orderId,
+            'orderId' => $orderId,
+            'signature' => $signature,
+            'lang' => 'vi',
+        ];
+
+        $response = Http::post('https://test-payment.momo.vn/v2/gateway/api/query', $requestBody);
+
+        $responseData = $response->json();
+
+        if ($responseData['resultCode'] == 0) {
+            $booking = Booking::where('id', $bookingId)->first();
+
+            if ($booking) {
+                $booking->status = 'Thanh toán thành công';
+                $booking->save();
+            }
+        }
+        return ApiResponse(true, $responseData, Response::HTTP_OK, messageResponseData());
     }
 }
--- routes/momo.php
+++ routes/momo.php
@@ -2,9 +2,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\Momo\MomoController;
 
-Route::group(['prefix' => 'user/momo'], function () {
-
+Route::group(['prefix' => 'client/momo'], function () {
     //Create Momo Route
-    Route::post('/create',[MomoController::class,'createPayment']);
+    Route::post('/create',[MomoController::class,'payment']);
+    // call back
+    Route::post('/momo/callback', [MomoController::class, 'callback'])->name('momo.callback');
+    // payment success
+    Route::post('/success', [MomoController::class, 'checkStatusTransaction'])->name('payment.success');
+
 
 });

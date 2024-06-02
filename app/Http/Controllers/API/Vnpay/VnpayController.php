35aee9ff4ffd7fcb705efbf16fa5b2100bb5e4d8
PTuanDEV <tuanchay.ko@gmail.com>
2024-06-02 17:20:09 +0700

    Gọi môi trường test vnpay

---
+++ app/Http/Controllers/API/Vnpay/VnpayController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\API\Vnpay;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Response;
+use Illuminate\Http\Request;
+
+class VnpayController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum');
+    }
+
+    //POST api/pay/vnpay (truyền booking_id, price)
+    public function index(Request $request)
+    {
+        try {
+            $vnp_Url = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
+            $vnp_Returnurl = "http://127.0.0.1:8000";
+            $vnp_TmnCode = "5N23P3P2";
+            $vnp_HashSecret = "IXXRTPQNFDPFJHDKXSUJZOJURZQLMJIK";
+            $vnp_TxnRef = $request->booking_id; // $request->booking_id
+            $vnp_OrderInfo = "Thanh toán vé xem phim";
+            $vnp_OrderType = "billpayment";
+            $vnp_Amount = $request->price * 100; //$request->price
+            $vnp_Locale = "vn";
+            $vnp_BankCode = "NCB";
+            $vnp_IpAddr = $_SERVER['REMOTE_ADDR'];
+            $inputData = array(
+                "vnp_Version" => "2.1.0",
+                "vnp_TmnCode" => $vnp_TmnCode,
+                "vnp_Amount" => $vnp_Amount,
+                "vnp_Command" => "pay",
+                "vnp_CreateDate" => date('YmdHis'),
+                "vnp_CurrCode" => "VND",
+                "vnp_IpAddr" => $vnp_IpAddr,
+                "vnp_Locale" => $vnp_Locale,
+                "vnp_OrderInfo" => $vnp_OrderInfo,
+                "vnp_OrderType" => $vnp_OrderType,
+                "vnp_ReturnUrl" => $vnp_Returnurl,
+                "vnp_TxnRef" => $vnp_TxnRef,
+            );
+            if (isset($vnp_BankCode) && $vnp_BankCode != "") {
+                $inputData['vnp_BankCode'] = $vnp_BankCode;
+            }
+            if (isset($vnp_Bill_State) && $vnp_Bill_State != "") {
+                $inputData['vnp_Bill_State'] = $vnp_Bill_State;
+            }
+            ksort($inputData);
+            $query = "";
+            $i = 0;
+            $hashdata = "";
+            foreach ($inputData as $key => $value) {
+                if ($i == 1) {
+                    $hashdata .= '&' . urlencode($key) . "=" . urlencode($value);
+                } else {
+                    $hashdata .= urlencode($key) . "=" . urlencode($value);
+                    $i = 1;
+                }
+                $query .= urlencode($key) . "=" . urlencode($value) . '&';
+            }
+            $vnp_Url = $vnp_Url . "?" . $query;
+            if (isset($vnp_HashSecret)) {
+                $vnpSecureHash =   hash_hmac('sha512', $hashdata, $vnp_HashSecret); //  
+                $vnp_Url .= 'vnp_SecureHash=' . $vnpSecureHash;
+            }
+            return redirect($vnp_Url);
+        } catch (\Exception $e) {
+            return ApiResponse(false, null, Response::HTTP_BAD_GATEWAY, $e->getMessage());
+        }
+    }
+}
---
+++ routes/vnpay.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Http\Controllers\API\Vnpay\VnpayController;
+use Illuminate\Support\Facades\Route;
+
+Route::group(['prefix' => 'pay/vnpay'], function () {
+    // GET All User Route
+    Route::post('/',[VnpayController::class,'index']);
+});
